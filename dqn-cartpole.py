8b17bfa091eca0114d5e7396b1f69ce37fc6186c
thtrieu <thtrieu@apcs.vn>
2017-01-10 03:34:51 +0700

    hyper parameters

--- dqn-cartpole.py
+++ dqn-cartpole.py
@@ -5,15 +5,15 @@ from src.utils import uniform
 env = CartPole()
 observe = env.appearance()
 
-inp_dim = len(observe) + 1
-hid_dim = 4
 action_space = [[-1.0], [1.0]]
+inp_dim = len(observe) + len(action_space[0])
+hid_dim = 4
+# Q function is appproximated by depth-2 MLP
 cart = Agent((inp_dim, hid_dim, 1), action_space)
 
 total = int(1e5)
 for count in range(total):
     epsilon = max(1. - count * 1.0 / total, .1)
-    # epsilon = .1
     action, q_val = cart.act(observe, epsilon)
     if action is None: # exploration
         action = action_space[round(uniform())]
@@ -23,10 +23,12 @@ for count in range(total):
     transition = [observe + action, reward, new_observe]
     cart.store_and_learn(transition)
     observe = new_observe
-    if (count + 1) % 100 == 0:
+
+    if (count + 1) % 8 == 0:
         cart.update()
-        print(q_val)
+        if q_val: print('Q = ', q_val)
     
     if (count + 1) % 1500 == 0:
         cart.save('cart_{}'.format(count + 1))
+    
     if env.viz(1) == 27: break
