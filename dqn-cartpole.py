967d43a00d5a85b332aefc6ea2ea49662e421fec
thtrieu <thtrieu@apcs.vn>
2017-01-10 04:17:14 +0700

    readability

--- dqn-cartpole.py
+++ dqn-cartpole.py
@@ -5,11 +5,11 @@ from src.utils import uniform
 env = CartPole()
 observe = env.appearance()
 
+# Q function is appproximated by depth-2 MLP
 action_space = [[-1.0], [1.0]]
 inp_dim = len(observe) + len(action_space[0])
-hid_dim = 4
-# Q function is appproximated by depth-2 MLP
-cart = Agent((inp_dim, hid_dim, 1), action_space)
+MLP_dims = (inp_dim, 4, 1)
+cart = Agent(MLP_dims, action_space)
 
 total = int(1e5)
 for count in range(total):
@@ -23,12 +23,11 @@ for count in range(total):
     transition = [observe + action, reward, new_observe]
     cart.store_and_learn(transition)
     observe = new_observe
-
+    
     if (count + 1) % 8 == 0:
         cart.update()
         if q_val: print('Q = ', q_val)
     
     if (count + 1) % 1500 == 0:
         cart.save('cart_{}'.format(count + 1))
-    
     if env.viz(1) == 27: break
